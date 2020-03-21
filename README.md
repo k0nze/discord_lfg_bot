447d69817259b5309ed36d6d3c80327a26448303
Konstantin Luebeck <konstantin.luebeck@uni-tuebingen.de>
2020-03-21 11:26:01 +0100

    Implemented checking of LFG command, DM error message and creation instant invite to a voice channel.

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # Discord LFG Bot
 ## Setup
+
+Create a Discord application and bot in the Discord developer portal and make sure to set the following persmissions:
+
+ * General Permissions
+    * Manage Channels
+    * Create Instant Invite
+    * View Channels
+
+ * Text Permissions
+    * Send Messages
+    * Manage Messages
+    * Embed Links
+    * Mention Everyone
+    * Use External Emoji
+    * Add Reactions
+
+Under OAuth check _bot_.
+
 Install necessary packages:
 
 ```bash
@@ -14,8 +32,41 @@ cp .env.sample .env
 
 Copy the `config.json.sample` to `config.json` and Replace the following placeholders
 
-### `{lfg_channel_id}`
-ID of the channel to which the bot should listen to the command
+### `command_prefix`
+
+On which command prefix should the bot be triggered
+
+### `lfg_command`
+
+Command after `command_prefix` which will generate a voice channel invite
+
+### `lfg_channel_id`
+
+ID of the channel to which the bot should listen to the LFG command: `command_prefix``lfg_command`
+
+### `command_synopsis`
+
+Explains how an LFG command should be structured. This will be send as a DM to a member who used command wrong
+
+### `dm_command_hint`
+
+Text that will be send before the `command_synopsis` in a DM
+
+### `dm_no_role_mentioned`
+
+Text that will be send before `dm_command_hint` in a DM if no role was mentioned in the LFG command
+
+### `dm_no_lfg_message`
+
+Text that will be send before `dm_command_hint` in a DM if no message was added to the LFG command 
+
+### `dm_not_in_voice_channel`
+
+Text that will be send before `dm_command_hint` in a DM if author of LFG command is not in a voice channel 
+
+### `invite_max_age`
+
+How long should an invite be valid in seconds
 
 
 ## Run
--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import discord
 import json
+import re
 
 from datetime import datetime
 from discord.utils import get
@@ -38,6 +39,19 @@ lfg_bot = commands.Bot(command_prefix='!')
 lfg_command = get_config()['lfg_command']
 
 
+async def send_command_error_message(member, additional_content):
+    command_synopsis = get_config()['command_synopsis']
+    command_synopsis = command_synopsis.replace('{command_prefix}', command_prefix)
+    command_synopsis = command_synopsis.replace('{lfg_command}', lfg_command)
+
+    dm_command_hint = get_config()['dm_command_hint']
+
+    message_text = additional_content + "\n" + dm_command_hint + "\n" + command_synopsis
+
+    channel = await member.create_dm()
+    await channel.send(message_text)
+
+
 @lfg_bot.event
 async def on_ready():
 
@@ -59,13 +73,87 @@ async def add_id(ctx):
     guild = ctx.guild
     channel = ctx.channel
     author = ctx.author
+    message = ctx.message
     command = ctx.message.content
 
     log_message(guild, f'"{command_prefix}{lfg_command}" executed by "{author}" in "{channel}#{channel.id}"')
 
-        # delete message containing the command
-    #await ctx.message.delete()
+    # check if command was issued in the correct channel
+    lfg_channel_id = get_config()['lfg_channel_id']
 
+    if channel.id != lfg_channel_id:
+        log_message(guild, f'* command was ignored since it was not issued in the correct channel')
+        return
+
+    # check if command contains a role mention
+    if len(message.role_mentions) < 1:
+        dm_no_role_mentioned = get_config()['dm_no_role_mentioned'] 
+        await send_command_error_message(author, dm_no_role_mentioned)
+        await message.delete()
+        log_message(guild, f'* command message was deleted since no role was mentioned in it')
+        return
+       
+    # check if command contains a message
+    lfg_message = command.replace(command_prefix, "")
+    lfg_message = lfg_message.replace(lfg_command, "")
+    lfg_message = re.sub('<@&[0-9]+>', '', lfg_message)
+    lfg_message = lfg_message.lstrip()
+
+    if len(lfg_message) == 0:
+        dm_no_lfg_message = get_config()['dm_no_lfg_message'] 
+        await send_command_error_message(author, dm_no_lfg_message)
+        await message.delete()
+        log_message(guild, f'* command message was deleted since no message was in it')
+        return
+
+    # check if command issuer is in a voice channel
+    author_voice_channel = None
+
+    for voice_channel in guild.voice_channels:
+        if author_voice_channel is None: 
+            for member in voice_channel.members:
+                if member.id == author.id:
+                    author_voice_channel = voice_channel
+                    break;
+           
+    if author_voice_channel is None:
+        dm_not_in_voice_channel = get_config()['dm_not_in_voice_channel'] 
+        await send_command_error_message(author, dm_not_in_voice_channel)
+        await message.delete()
+        log_message(guild, f'* command message was deleted since author is not in a voice channel')
+        return
+
+    voice_channel_invite = await author_voice_channel.create_invite(max_age=int(get_config()['invite_max_age']))
+
+    # generate invite message
+    invite_message = ""
+
+    # add mentioned roles to invite message
+    for role_mention in message.role_mentions:
+        invite_message = invite_message + f'<@&{role_mention.id}> '
+
+    # add lfg message to invite message
+    invite_message = invite_message + lfg_message + "\nfrom " 
+
+    # add member which are in the voice channel to invite message
+    for member in author_voice_channel.members:
+        invite_message = invite_message + f'<@!{member.id}> '
+
+    invite_message = invite_message 
+
+    # send invite message
+    await channel.send(invite_message)
+
+    # send voice channel invite
+    await channel.send(voice_channel_invite)  
+
+    # delete message containting the command 
+    await message.delete()
+
+    log_message(guild, f'* invite has been created')
+
+
+# TODO delete messages in lfg channel that don't contain a command
 
 
 lfg_bot.run(DISCORD_TOKEN)
