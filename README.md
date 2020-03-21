2c3544397d264fa178c7a69459bbc5ae44d71fc4
Konstantin Luebeck <konstantin.luebeck@uni-tuebingen.de>
2020-03-21 08:58:57 +0100

    Bot listens to the its command.

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # Discord LFG Bot
 ## Setup
-
 Install necessary packages:
 
 ```bash
 python3 -m pip install -r requirements.txt
 ```
 
-Copy the `.env.sample` and write your bots Discord token in `.env`
+Copy the `.env.sample` and replace `{discord-token}` with your bot's Discord token in `.env`
 
 ```bash
 cp .env.sample .env
 ```
+
+Copy the `config.json.sample` to `config.json` and Replace the following placeholders
+
+### `{lfg_channel_id}`
+ID of the channel to which the bot should listen to the command
+
 
 ## Run
 ```bash
--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import json
 
 from datetime import datetime
 from discord.utils import get
@@ -14,15 +15,28 @@ dotenv_path = join(dir_path, '.env')
 load_dotenv(dotenv_path)
 
 DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
-
-
-lfg_bot = commands.Bot(command_prefix='!lfg')
+CONFIG_JSON = os.environ.get('CONFIG_JSON')
 
 
 def log_message(guild, message):
     datetime_now = datetime.now()
     print(str(datetime_now) + " - " + f'{guild.name}#{guild.id}: {message}')
- 
+
+
+def get_config():
+    config = None
+    with open(CONFIG_JSON) as config_json:
+        config = json.load(config_json)
+
+    return config
+
+
+# setup command prefix
+command_prefix = get_config()['command_prefix']
+lfg_bot = commands.Bot(command_prefix='!')
+
+lfg_command = get_config()['lfg_command']
+
 
 @lfg_bot.event
 async def on_ready():
@@ -38,6 +52,20 @@ async def on_ready():
 
     # set activitiy
     await lfg_bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="git.io/JvMhj"))
+
+
+@lfg_bot.command(name=lfg_command)
+async def add_id(ctx):
+    guild = ctx.guild
+    channel = ctx.channel
+    author = ctx.author
+    command = ctx.message.content
+
+    log_message(guild, f'"{command_prefix}{lfg_command}" executed by "{author}" in "{channel}#{channel.id}"')
+
+        # delete message containing the command
+    #await ctx.message.delete()
+
 
 
 lfg_bot.run(DISCORD_TOKEN)
